cecca645ce94a86de6f0dda5fbd9dd16cd800bff
Chiraag Chari <chiraagchari011@gmail.com>
2025-10-08 16:56:34 +0530

    SimpleDartProgram.dart

--- Ex.2/SimpleDartProgram.dart
+++ Ex.2/SimpleDartProgram.dart
@@ -1,14 +1,13 @@
 import 'dart:io';
 
 void main() {
-  // Taking input
   stdout.write('Enter a number: ');
   int number = int.parse(stdin.readLineSync()!);
 
   print('Counting from 1 to $number:');
 
-  // Using a for loop
   for (int i = 1; i <= number; i++) {
     print(i);
   }
 }
+
